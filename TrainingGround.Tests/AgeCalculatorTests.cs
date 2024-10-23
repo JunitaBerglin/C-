18e615a10f2d5da5d58b9b82103480e352f90b01
Junita Berglin <junita.berglin@gmail.com>
2024-10-23 11:31:02 +0200

    ✅ (feat): test success

--- TrainingGround.Tests/AgeCalculatorTests.cs
+++ TrainingGround.Tests/AgeCalculatorTests.cs
@@ -2,9 +2,19 @@ namespace TrainingGround.Tests;
 
 public class AgeCalculatorTests
 {
+    private AgeCalculator _calculator;
+
+    public AgeCalculatorTests()
+    {
+        _calculator = new AgeCalculator();
+    }
+
     [Fact]
     public void someone_born_1972_is_50_in_2022()
     {
 
+        var age = _calculator.GetAge(1972, 2022);
+
+        Assert.Equal(50, age);
     }
 }
--- TrainingGround/AgeCalculator.cs
+++ TrainingGround/AgeCalculator.cs
@@ -1,8 +1,8 @@
 ﻿namespace TrainingGround;
 public class AgeCalculator
 {
-    public int GetAge(int birthYear, int cirrentYear)
+    public int GetAge(int birthYear, int currentYear)
     {
-        return 0;
+        return currentYear - birthYear;
     }
 }
